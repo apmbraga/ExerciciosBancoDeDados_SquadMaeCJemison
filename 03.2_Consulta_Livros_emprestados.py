3d52216fab4bd3fd6f929ad815fa80514a5f0f08
apmbraga <braga.ap95@gmail.com>
2024-02-06 17:09:39 -0300

    Acréscimo de Popula e ajustes em algumas querys

--- 03.2_Consulta_Livros_emprestados.py
+++ 03.2_Consulta_Livros_emprestados.py
@@ -4,10 +4,10 @@ conexao = sqlite3.connect('biblioteca')
 cursor = conexao.cursor()
 
 
-#CONSULTA 2:  Encontrar todos os livros emprestados no momento.
+#CONSULTA 2:  Encontrar todos os livros emprestados no momento, com TITULO DO LIVRO, QUEM PEGOU O LIVRO, DATA DO EMPRÉSTIMO E PRAZO PARA DEVOLUÇÃO
 emprestados = cursor.execute('SELECT livros.titulo, cadastro.nome, emprestimos.data_emprestimo, emprestimos.data_devolucao from emprestimos INNER JOIN livros ON emprestimos.id_livro = livros.id_livro INNER JOIN cadastro ON emprestimos.id_usuario = cadastro.id_usuario where FG_devolvido = 0')
+print('Titulo | Usuário com o Livro | Data do Empréstimo | Prazo para Devolução:')
 for _livrosindisponiveis in emprestados:
-    print('Titulo | Usuário com o Livro | Data do Empréstimo | Prazo para Devolução:')
     print(_livrosindisponiveis) 
 
 
